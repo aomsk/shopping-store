daee255f9b9f6d6c6072f7d2aecb7a4e04b04d42
Sipang K <plaaom39@gmail.com>
2023-07-15 19:28:25 +0700

    feat: see product detail by id

--- src/views/ProductDetail.tsx
+++ src/views/ProductDetail.tsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Products } from "../utils/interface";
+import { AiFillStar } from "react-icons/ai";
 
 function ProductDetail() {
   const { id } = useParams();
@@ -27,8 +28,25 @@ function ProductDetail() {
   });
 
   return (
-    <div className="">
-      <h1 className="text-xl font-bold">{product?.title}</h1>
+    <div className="grid grid-cols-1 xl:grid-cols-2 xl:px-10 xl:pt-10">
+      <div className="flex justify-center">
+        <img src={product?.image} alt="" className="w-[150px] xl:w-[300px] h-fit pt-2" />
+      </div>
+      <div className="p-10">
+        <h1 className="text-4xl font-bold">{product?.title}</h1>
+        <div className="badge badge-outline mt-5">{product?.category}</div>
+        <span className="flex items-center mt-5">
+          <AiFillStar className="mr-1 text-2xl" />
+          {product?.rating.rate}
+        </span>
+        <p className="mt-5">{product?.description}</p>
+        <div className="flex items-end mt-10">
+          <p className="text-2xl font-bold mr-10">Price: {product?.price}$</p>
+          <button type="button" className="btn">
+            Buy Now
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
